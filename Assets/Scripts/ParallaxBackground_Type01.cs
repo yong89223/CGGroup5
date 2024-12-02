c0ed58e3be05c439259dca861e2ed3774d09a911
ThisGrDn <fhqht0638@naver.com>
2024-12-03 00:12:18 +0900

    fix(scene/scripts)
    First, Lobby, Select, Store Screen Parallax background 적용 완료

--- Assets/Scripts/ParallaxBackground_Type01.cs
+++ Assets/Scripts/ParallaxBackground_Type01.cs
@@ -10,14 +10,14 @@ public class ParallaxBackground_Type01 : MonoBehaviour
     private Vector3 moveDirection;           // 이동 방향
 
     private float backgroundWidth;           // 배경의 너비
-    private Vector3 startPosition;           // 초기 위치 저장
+    private Vector3 initialPosition;         // 초기 위치 저장
 
     private void Start()
     {
         // 배경이 RectTransform을 사용하는 경우
         RectTransform rectTransform = GetComponent<RectTransform>();
         backgroundWidth = rectTransform.rect.width;  // RectTransform의 너비 사용
-        startPosition = transform.position;          // 초기 위치 저장
+        initialPosition = transform.position;         // 초기 위치 저장
     }
 
     private void Update()
@@ -25,17 +25,11 @@ public class ParallaxBackground_Type01 : MonoBehaviour
         // 배경이 moveDirection 방향으로 moveSpeed의 속도로 이동
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
-        // 배경이 왼쪽 방향으로 이동해서 화면을 벗어나면
-        if (transform.position.x <= startPosition.x - backgroundWidth)
+        // 배경이 왼쪽으로 이동하여 화면을 벗어나면
+        if (transform.position.x <= -backgroundWidth/2)
         {
             // 배경을 오른쪽 끝으로 이동
-            transform.position = new Vector3(startPosition.x + backgroundWidth, transform.position.y, transform.position.z);
-        }
-        // 배경이 오른쪽 방향으로 이동해서 화면을 벗어나면
-        else if (transform.position.x >= startPosition.x + backgroundWidth)
-        {
-            // 배경을 왼쪽 끝으로 이동
-            transform.position = new Vector3(startPosition.x - backgroundWidth, transform.position.y, transform.position.z);
+            transform.position += new Vector3(backgroundWidth * 2, 0, 0);
         }
     }
 }
